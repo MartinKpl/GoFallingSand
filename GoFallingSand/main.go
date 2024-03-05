41b2150e6c19b79f03eccead551cc09fb3c4a3b3
MartinKpl <martin.kaplan910@gmail.com>
2024-03-05 10:59:40 +0200

    Better falling to sides

--- GoFallingSand/main.go
+++ GoFallingSand/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image/color"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
@@ -26,9 +27,30 @@ func (g *Game) Update() error {
 	nextGrid := grid
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[i]); j++ {
-			if grid[i][j] && j+1 < len(grid[i]) && !grid[i][j+1] {
-				nextGrid[i][j] = false
-				nextGrid[i][j+1] = true
+			if grid[i][j] {
+				if j+1 < len(grid[i]) && !grid[i][j+1] { //if there is not a sand grain or the floor below, move the grain there
+					nextGrid[i][j] = false
+					nextGrid[i][j+1] = true
+				} else if j+1 < len(grid[i]) && grid[i][j+1] { //if there is a sand grain below and is not the floor move to a side
+					leftEmpty := i-1 >= 0 && !grid[i-1][j+1]
+					rightEmpty := i+1 < len(grid) && !grid[i+1][j+1]
+
+					if leftEmpty && rightEmpty {
+						if rand.Intn(2) == 1 { //random left or right
+							leftEmpty = false
+						} else {
+							rightEmpty = false
+						}
+					}
+
+					if leftEmpty {
+						nextGrid[i][j] = false
+						nextGrid[i-1][j+1] = true
+					} else if rightEmpty {
+						nextGrid[i][j] = false
+						nextGrid[i+1][j+1] = true
+					}
+				}
 			}
 		}
 	}
